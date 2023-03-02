13e6e10cf6e1a1736fec8e14fe738372fb6f47fd
Vladyslav Polishchuk <105670228+vladpolischchuk@users.noreply.github.com>
2023-03-02 19:00:26 +0200

    Update Modal.jsx

--- src/shared/components/modal/Modal.jsx
+++ src/shared/components/modal/Modal.jsx
@@ -1,10 +1,35 @@
-import { Component } from "react";
+import { useEffect } from "react";
 import { createPortal } from "react-dom";
 
 import css from './Modal.module.css';
 
 const modalRoot = document.querySelector("#modal-root");
 
+const Modal = ({ closeImage, children }) => {
+    const closeModal = ({ target, currentTarget, code }) => {
+        if (target === currentTarget || code === "Escape") {
+            closeImage();
+        };
+    };
+
+    useEffect(() => {
+        document.addEventListener("keydown", closeModal);
+        return () => document.removeEventListener("keydown", closeModal);
+    });
+
+    return (
+        createPortal(
+            <div className={css.Overlay} onClick={closeModal}>
+                <div className={css.Modal}>
+                    {children}
+                </div>
+            </div>,
+            modalRoot
+        )
+    );
+};
+
+/*
 class Modal extends Component {
 
     componentDidMount() {
@@ -37,5 +62,6 @@ class Modal extends Component {
         )
     };
 };
+*/
 
 export default Modal;
